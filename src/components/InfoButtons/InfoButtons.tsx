e1a8b546f38b4e66cde469adc8ec67a1bbcdd844
Jaden Chant <chant1@stolaf.edu>
2024-03-04 12:35:54 -0600

    added react-router

--- src/components/InfoButtons/InfoButtons.tsx
+++ src/components/InfoButtons/InfoButtons.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import block from 'bem-cn-lite';
 import {Button, Icon} from '@gravity-ui/uikit';
-import {BookOpen, CopyTransparent, ListUl, LogoTelegram} from '@gravity-ui/icons';
-import Figma from '../../assets/icons/figma.svg?react';
-import Storybook from '../../assets/icons/storybook.svg?react';
-import GitHub from '../../assets/icons/github.svg?react';
+import {CopyTransparent, ListUl} from '@gravity-ui/icons';
 
 import './InfoButtons.scss';
 
@@ -28,70 +25,9 @@ export const InfoButtons: React.FC = () => {
                         </Button>
                     </div>
                     <div className={b('button')}>
-                        <Button
-                            size="l"
-                            view="outlined"
-                            href="https://gravity-ui.com/libraries"
-                            target="_blank"
-                        >
+                        <Button size="l" href="https://gravity-ui.com/libraries" target="_blank">
                             <Icon data={ListUl} />
                             Libraries
-                        </Button>
-                    </div>
-                    <div className={b('button')}>
-                        <Button
-                            size="l"
-                            view="outlined"
-                            href="https://preview.gravity-ui.com/uikit/"
-                            target="_blank"
-                        >
-                            <Icon data={Storybook} size={16} />
-                            Storybook
-                        </Button>
-                    </div>
-                    <div className={b('button')}>
-                        <Button
-                            size="l"
-                            view="outlined"
-                            href="https://github.com/gravity-ui"
-                            target="_blank"
-                        >
-                            <Icon data={GitHub} size={16} />
-                            GitHub
-                        </Button>
-                    </div>
-                    <div className={b('button')}>
-                        <Button
-                            size="l"
-                            view="outlined"
-                            href="https://www.figma.com/community/file/1271150067798118027/Gravity-UI-Design-System-(Beta)"
-                            target="_blank"
-                        >
-                            <Icon data={Figma} size={18} />
-                            Figma
-                        </Button>
-                    </div>
-                    <div className={b('button')}>
-                        <Button
-                            size="l"
-                            view="outlined"
-                            href="https://t.me/gravity_ui"
-                            target="_blank"
-                        >
-                            <Icon data={LogoTelegram} />
-                            Telegram
-                        </Button>
-                    </div>
-                </div>
-            </div>
-
-            <div className={b('block')}>
-                <div className={b('title')}>About Vite</div>
-                <div className={b('buttons')}>
-                    <div className={b('button')}>
-                        <Button size="l" view="outlined" href="https://vitejs.dev" target="_blank">
-                            <Icon data={BookOpen} />
-                            Docs
                         </Button>
                     </div>
                 </div>
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,14 @@ import '@gravity-ui/uikit/styles/fonts.css';
 import '@gravity-ui/uikit/styles/styles.css';
 import './styles/globals.scss';
 import App from './App';
+// import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+// const router = createBrowserRouter([
+//     {
+//         path: '/',
+//         element: <div>Hello world!</div>,
+//     },
+// ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
