9c4a5fd138f8be8832a1e386a091a4c0a65ae027
Jaden Chant <chant1@stolaf.edu>
2024-03-05 20:27:16 -0600

    Add styling to 404 page and display table on student home page

--- src/pages/student/home.tsx
+++ src/pages/student/home.tsx
@@ -1,5 +1,20 @@
+import {Table, TableSettingsData, withTableSettings} from '@gravity-ui/uikit';
+import {useState} from 'react';
+
+const data = [
+  {major: 'CS', status: 'in progress', updated: 'Date'},
+  {major: 'Math', status: 'complete', updated: 'Date'},
+];
+
+const col = [{id: 'major'}, {id: 'status'}, {id: 'updated'}];
+
 const StudentHome = () => {
-  return <div>Hi there Student</div>;
+  return (
+    <section>
+      <div>St. Olaf POS</div>
+      <Table data={data} columns={col} className="" />
+    </section>
+  );
 };
 
 export default StudentHome;
