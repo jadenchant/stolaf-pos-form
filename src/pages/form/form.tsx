b35ff5810d6f6bba7ac5aa166d6a775664cc3745
Jaden Chant <chant1@stolaf.edu>
2024-03-11 08:51:56 -0500

    Add other elective options to the form

--- src/pages/form/form.tsx
+++ src/pages/form/form.tsx
@@ -13,6 +13,23 @@ const electiveData = [
   {id: 'csci333', name: 'Theory of Computation', prerequisite: ['math234']},
 ];
 
+const otherElectiveData = [
+  {id: 'csci200', name: 'Topics in CS', prerequisite: ['varies']},
+  {id: 'csci300', name: 'Topics in CS', prerequisite: ['varies']},
+  {id: 'csci284', name: 'Mobile Computing Applications', prerequisite: ['csci251']},
+  {id: 'csci336', name: 'Logic Programming', prerequisite: ['csci251']},
+  {id: 'csci356', name: 'Parallel and Distributed Systems', prerequisite: ['csci241', 'csci251']},
+  {id: 'csci390', name: 'Senior Capstone', prerequisite: ['csci263', 'csci353']},
+  {id: 'csci391', name: 'Senior Capstone', prerequisite: ['csci263', 'csci353']},
+  {id: 'math282', name: 'Computational Geometry', prerequisite: ['none']},
+  {
+    id: 'mscs341',
+    name: 'Algorithms for Decision Making',
+    prerequisite: ['csci251', 'math220', 'stat272'],
+  },
+  {id: 'phys246', name: 'Electronics', prerequisite: ['phys125', 'phys131']},
+];
+
 const formatID = (id: string) => {
   const matchResult = id.match(/[a-zA-Z]+/);
   const prefix = matchResult ? matchResult[0].toUpperCase() : '';
@@ -20,13 +37,26 @@ const formatID = (id: string) => {
   return `${prefix} ${num}`;
 };
 
-const electiveSelect = (
-  <Select width="max" size="l" multiple filterable hasClear>
-    {electiveData.map((item) => (
-      <Select.Option value={item.id}>{`${formatID(item.id)}: ${item.name}`}</Select.Option>
-    ))}
-  </Select>
-);
+const electiveSelect = (data: any[]) => {
+  return (
+    <Select width="max" size="l" multiple filterable hasClear>
+      {data.map((item, outerIndex) => {
+        return (
+          <Select.Option value={formatID(item.id)} key={outerIndex}>
+            <div className="w-full flex justify-between">
+              <p>{`${formatID(item.id)}: ${item.name}`}</p>
+              <div>
+                {item.prerequisite.map((prereq: string, innerIndex: any) => {
+                  return <p key={`${outerIndex}-${innerIndex}`}>{formatID(prereq)}</p>;
+                })}
+              </div>
+            </div>
+          </Select.Option>
+        );
+      })}
+    </Select>
+  );
+};
 
 const Form = () => {
   const navigate = useNavigate();
@@ -36,7 +66,14 @@ const Form = () => {
       <div className="">
         <h2 className="text-2xl font-bold mt-8">Primary Classes</h2>
         <h2 className="text-2xl font-bold mt-4">Electives</h2>
-        {electiveSelect}
+        <h3>
+          <span className="font-bold">Designated:</span> Select at least 2 classes
+        </h3>
+        {electiveSelect(electiveData)}
+        <h3>
+          <span className="font-bold">Other Electives</span>
+        </h3>
+        {electiveSelect(otherElectiveData)}
         <div className="flex justify-between mt-4">
           <Button view="normal" size="l" onClick={() => navigate(-1)}>
             Cancel
