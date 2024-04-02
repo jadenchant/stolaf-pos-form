135a642c86388bc6605c1902dc6198851b7de3ce
Jaden Chant <chant1@stolaf.edu>
2024-04-02 18:52:14 -0500

    Refactor formatID function to use 'or' and 'and' instead of '||' and '&&'

--- src/pages/form/FormatID.ts
+++ src/pages/form/FormatID.ts
@@ -2,8 +2,10 @@ const formatID = (id: string) => {
   return id
     .split(' ')
     .map((part) => {
-      if (part === '||' || part === '&&') {
-        return part;
+      if (part === '||' || part === 'or') {
+        return 'or';
+      } else if (part === '&&' || part === 'and') {
+        return 'and';
       } else {
         const charMatch = part.match(/[a-zA-Z]+/);
         const prefix = charMatch ? charMatch[0].toUpperCase() : '';
