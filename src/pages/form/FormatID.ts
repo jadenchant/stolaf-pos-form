2b1ed4216fa7e9b1a4a8ca6069f4714504504de0
Jaden Chant <chant1@stolaf.edu>
2024-04-02 19:03:24 -0500

    Refactor formatID function to handle special cases and improve readability

--- src/pages/form/FormatID.ts
+++ src/pages/form/FormatID.ts
@@ -2,16 +2,28 @@ const formatID = (id: string) => {
   return id
     .split(' ')
     .map((part) => {
+      part = part.toLowerCase();
+
       if (part === '||' || part === 'or') {
         return 'or';
       } else if (part === '&&' || part === 'and') {
         return 'and';
+      } else if (part === 'none' || part === 'na') {
+        return ' ';
       } else {
         const charMatch = part.match(/[a-zA-Z]+/);
-        const prefix = charMatch ? charMatch[0].toUpperCase() : '';
-        const numMatch = part.match(/[0-9]+/);
-        const suffix = numMatch ? numMatch[0] : '';
-        return `${prefix} ${suffix}`;
+        if (
+          charMatch &&
+          charMatch[0].length >= 3 &&
+          charMatch[0].length <= 4
+        ) {
+          const prefix = charMatch[0].toUpperCase();
+          const numMatch = part.match(/[0-9]+/);
+          const suffix = numMatch ? numMatch[0] : '';
+          return `${prefix} ${suffix}`;
+        }
+        part = part.charAt(0).toUpperCase() + part.slice(1);
+        return part;
       }
     })
     .join(' ');
