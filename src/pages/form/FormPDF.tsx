6ae03105b6660ada1fa9fef556e005c11cabc2d4
Jaden Chant <chant1@stolaf.edu>
2024-04-08 08:52:30 -0500

    Refactor FormPDF component to use flexbox for layout, add title, and display foundational courses in PDF

--- src/pages/form/FormPDF.tsx
+++ src/pages/form/FormPDF.tsx
@@ -5,25 +5,50 @@ import {
   Document,
   StyleSheet,
 } from '@react-pdf/renderer';
+import {foundationData} from './CSFormData';
 
 const styles = StyleSheet.create({
   page: {
     display: 'flex',
     flexDirection: 'column',
   },
-  section: {
+  titleSection: {
     margin: 10,
-    padding: 10,
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
   },
   headText: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     margin: 10,
-    padding: 10,
   },
-  title: {
-    fontSize: 24,
+  section: {
+    margin: 10,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  table: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  tableClass: {
+    width: '45%',
+  },
+  tablePrereq: {
+    width: '35%',
+  },
+  tableTerm: {
+    width: '10%',
+  },
+  tableYear: {
+    width: '10%',
   },
 });
 
@@ -33,9 +58,12 @@ const data = {
 };
 
 export const FormPDF = () => (
-  <Document title="St. Olaf Program Of Study Form" language="english">
+  <Document
+    title="St. Olaf CS Program Of Study Form"
+    language="english"
+  >
     <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
+      <View style={styles.titleSection}>
         <Text style={styles.title}>
           Computer Science Major Plan (CSMaP)
         </Text>
@@ -45,8 +73,21 @@ export const FormPDF = () => (
         <Text>Expected graduation year: {data.gradYear}</Text>
       </View>
       <View style={styles.section}>
-        <Text>Section #2</Text>
+        <Text style={styles.bold}>Foundational Courses: </Text>
+        <Text>Must complete by the end of sophomore year.</Text>
       </View>
+      <View style={styles.table}>
+        <Text style={styles.tableClass}>Course</Text>
+        <Text style={styles.tablePrereq}>Prerequisite(s)</Text>
+        <Text style={styles.tableTerm}>Term</Text>
+        <Text style={styles.tableYear}>Year</Text>
+      </View>
+      {foundationData.map((course) => (
+        <View style={styles.section} key={course.id}>
+          <Text>{course.name}</Text>
+          <Text>Prerequisite: {course.prerequisite}</Text>
+        </View>
+      ))}
     </Page>
   </Document>
 );
