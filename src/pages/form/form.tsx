f993225800a6070cd2b7f7a0f398ad6aea253778
Jaden Chant <chant1@stolaf.edu>
2024-04-28 19:17:34 -0500

    Update rejection reasons and faculty information in form rendering

--- src/pages/form/form.tsx
+++ src/pages/form/form.tsx
@@ -100,185 +100,175 @@ const Form = () => {
         </Card>
       )}
 
-      <div className="">
-        <h2 className="text-2xl font-bold mt-8 mb-2">
-          Foundational Courses
-        </h2>
-        <p className="mb-2">
-          Must complete by the end of sophomore year.
-        </p>
-        <ClassTable
-          formStatus={formStatus}
-          selectedValues={foundationData}
-          formValues={formValues}
-          setFormValues={setFormValues}
-          classNames="mb-8"
-        />
-        <h2 className="text-2xl font-bold mt-8 mb-4">
-          Required Courses
-        </h2>
-        <p className="mb-2">
-          Generally completed by end of junior year, perhaps 1 for
-          senior year.
-        </p>
-        <ClassTable
-          formStatus={formStatus}
-          selectedValues={requiredData}
-          formValues={formValues}
-          setFormValues={setFormValues}
-          classNames="mb-8"
-        />
-        <h2 className="text-2xl font-bold mb-2">Electives</h2>
-        <p className="mb-2">
-          Complete 3, at least 1 must be 300-level
-        </p>
+      <h2 className="text-2xl font-bold mt-8 mb-2">
+        Foundational Courses
+      </h2>
+      <p className="mb-2">
+        Must complete by the end of sophomore year.
+      </p>
+      <ClassTable
+        formStatus={formStatus}
+        selectedValues={foundationData}
+        formValues={formValues}
+        setFormValues={setFormValues}
+        classNames="mb-8"
+      />
+      <h2 className="text-2xl font-bold mt-8 mb-4">
+        Required Courses
+      </h2>
+      <p className="mb-2">
+        Generally completed by end of junior year, perhaps 1 for
+        senior year.
+      </p>
+      <ClassTable
+        formStatus={formStatus}
+        selectedValues={requiredData}
+        formValues={formValues}
+        setFormValues={setFormValues}
+        classNames="mb-8"
+      />
+      <h2 className="text-2xl font-bold mb-2">Electives</h2>
+      <p className="mb-2">Complete 3, at least 1 must be 300-level</p>
 
-        <div className="flex justify-between mb-2 align-baseline">
-          <h2 className="text-lg font-bold">
-            Designated:{' '}
-            <span className="text-sm font-normal">
-              Select at least 2 classes
-            </span>
-          </h2>
-          <div className="flex flex-col justify-end mr-8">
-            <p className="h-5">Prerequisites</p>
+      <div className="flex justify-between mb-2 align-baseline">
+        <h2 className="text-lg font-bold">
+          Designated:{' '}
+          <span className="text-sm font-normal">
+            Select at least 2 classes
+          </span>
+        </h2>
+        <div className="flex flex-col justify-end mr-8">
+          <p className="h-5">Prerequisites</p>
+        </div>
+      </div>
+
+      <ElectiveSelect
+        formStatus={formStatus}
+        classes={electiveData}
+        formValues={formValues}
+        setFormValues={setFormValues}
+      />
+
+      <ClassTable
+        formStatus={formStatus}
+        selectedValues={electiveData.filter((elective) =>
+          formValues.some(
+            (formValue) =>
+              formatID(elective.id) === formatID(formValue.id),
+          ),
+        )}
+        formValues={formValues}
+        setFormValues={setFormValues}
+        classNames="mb-8"
+      />
+
+      <div className="flex justify-between mb-2 align-baseline">
+        <h2 className="text-lg font-bold">Other Electives</h2>
+        <div className="flex flex-col justify-end mr-8">
+          <p className="h-5">Prerequisites</p>
+        </div>
+      </div>
+
+      <ElectiveSelect
+        formStatus={formStatus}
+        classes={otherElectiveData}
+        formValues={formValues}
+        setFormValues={setFormValues}
+        isOtherElective
+      />
+
+      <ClassTable
+        formStatus={formStatus}
+        selectedValues={otherElectiveData.filter((elective) =>
+          formValues.some(
+            (formValue) =>
+              formatID(elective.id) === formatID(formValue.id),
+          ),
+        )}
+        formValues={formValues}
+        setFormValues={setFormValues}
+      />
+
+      <div className="flex justify-center mt-8">
+        <Button onClick={() => setOpenModal(true)} view="action">
+          Create Signature
+        </Button>
+      </div>
+
+      <Modal open={openModel} onClose={() => setOpenModal(false)}>
+        <div className="flex flex-col justify-center items-center m-4">
+          <SignatureCanvas
+            penColor="black"
+            canvasProps={{
+              width:
+                screenSize.width > 800 ? 800 : screenSize.width - 60,
+              height: 200,
+              className: 'bg-white',
+            }}
+            ref={sigCanvas as any}
+          />
+
+          <div className="flex justify-between mt-3 w-11/12 max-w-[400px]">
+            <Button
+              onClick={() => setOpenModal(false)}
+              size="l"
+              view="outlined-warning"
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={() => sigCanvas.current?.clear()}
+              size="l"
+              view="outlined-warning"
+            >
+              Clear
+            </Button>
+            <Button onClick={create} size="l" view="action">
+              Save
+            </Button>
           </div>
         </div>
+      </Modal>
 
-        <ElectiveSelect
-          formStatus={formStatus}
-          classes={electiveData}
-          formValues={formValues}
-          setFormValues={setFormValues}
-        />
-
-        <ClassTable
-          formStatus={formStatus}
-          selectedValues={electiveData.filter((elective) =>
-            formValues.some(
-              (formValue) =>
-                formatID(elective.id) === formatID(formValue.id),
-            ),
-          )}
-          formValues={formValues}
-          setFormValues={setFormValues}
-          classNames="mb-8"
-        />
-
-        <div className="flex justify-between mb-2 align-baseline">
-          <h2 className="text-lg font-bold">Other Electives</h2>
-          <div className="flex flex-col justify-end mr-8">
-            <p className="h-5">Prerequisites</p>
-          </div>
+      {imageURL && (
+        <div className="flex justify-center mt-8 bg-slate-100">
+          <img src={imageURL} alt="signature" className="signature" />
         </div>
+      )}
 
-        <ElectiveSelect
-          formStatus={formStatus}
-          classes={otherElectiveData}
-          formValues={formValues}
-          setFormValues={setFormValues}
-          isOtherElective
-        />
-
-        <ClassTable
-          formStatus={formStatus}
-          selectedValues={otherElectiveData.filter((elective) =>
-            formValues.some(
-              (formValue) =>
-                formatID(elective.id) === formatID(formValue.id),
-            ),
-          )}
-          formValues={formValues}
-          setFormValues={setFormValues}
-        />
-
-        <div className="flex justify-center mt-8">
-          <Button onClick={() => setOpenModal(true)} view="action">
-            Create Signature
-          </Button>
-        </div>
-
-        <Modal open={openModel} onClose={() => setOpenModal(false)}>
-          <div className="flex flex-col justify-center items-center m-4">
-            <SignatureCanvas
-              penColor="black"
-              canvasProps={{
-                width:
-                  screenSize.width > 800
-                    ? 800
-                    : screenSize.width - 60,
-                height: 200,
-                className: 'bg-white',
-              }}
-              ref={sigCanvas as any}
-            />
-
-            <div className="flex justify-between mt-3 w-11/12 max-w-[400px]">
-              <Button
-                onClick={() => setOpenModal(false)}
-                size="l"
-                view="outlined-warning"
-              >
-                Cancel
-              </Button>
-              <Button
-                onClick={() => sigCanvas.current?.clear()}
-                size="l"
-                view="outlined-warning"
-              >
-                Clear
-              </Button>
-              <Button onClick={create} size="l" view="action">
+      <div
+        className={`flex ${
+          formStatus !== 'submitted_for_review' &&
+          formStatus !== 'complete'
+            ? 'justify-between'
+            : 'justify-center'
+        } mt-20 mx-4`}
+      >
+        <Button view="normal" size="l" onClick={() => navigate(-1)}>
+          Cancel
+        </Button>
+        {formStatus !== 'submitted_for_review' &&
+          formStatus !== 'complete' && (
+            <Link to="/student">
+              <Button view="action" size="l">
                 Save
               </Button>
-            </div>
-          </div>
-        </Modal>
-
-        {imageURL && (
-          <div className="flex justify-center mt-8 bg-slate-100">
-            <img
-              src={imageURL}
-              alt="signature"
-              className="signature"
-            />
-          </div>
-        )}
-
-        <div
-          className={`flex ${
-            formStatus !== 'submitted_for_review' &&
-            formStatus !== 'complete'
-              ? 'justify-between'
-              : 'justify-center'
-          } mt-20 mx-4`}
-        >
-          <Button view="normal" size="l" onClick={() => navigate(-1)}>
-            Cancel
-          </Button>
-          {formStatus !== 'submitted_for_review' &&
-            formStatus !== 'complete' && (
-              <Link to="/student">
-                <Button view="action" size="l">
-                  Save
-                </Button>
-              </Link>
-            )}
-          {formStatus !== 'submitted_for_review' &&
-            formStatus !== 'complete' && (
-              <Link to="/student">
-                <Button view="action" size="l">
-                  Submit
-                </Button>
-              </Link>
-            )}
-        </div>
-
-        {/* Temporary To View PDF */}
-        <PDFViewer width="100%" height="800">
-          <FormPDF formValues={formValues} sigURL={imageURL} />
-        </PDFViewer>
+            </Link>
+          )}
+        {formStatus !== 'submitted_for_review' &&
+          formStatus !== 'complete' && (
+            <Link to="/student">
+              <Button view="action" size="l">
+                Submit
+              </Button>
+            </Link>
+          )}
       </div>
+
+      {/* Temporary To View PDF */}
+      <PDFViewer width="100%" height="800" className="mt-8">
+        <FormPDF formValues={formValues} sigURL={imageURL} />
+      </PDFViewer>
     </div>
   );
 };
